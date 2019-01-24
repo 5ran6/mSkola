463d35bfe62ee90018d987a4211aebb866afc643
5ran6 <5raan6@gmail.com>
2019-01-24 21:17:33 +0100

    Parents: EUREKA! Results done. Praise God

--- app/src/main/java/mountedwings/org/mskola_mgt/parent/ResultActivity.java
+++ app/src/main/java/mountedwings/org/mskola_mgt/parent/ResultActivity.java
@@ -42,17 +42,14 @@ import static mountedwings.org.mskola_mgt.SettingFlat.myPref;
 
 public class ResultActivity extends AppCompatActivity {
     private NestedScrollView nested_scroll_view;
-    private ImageButton bt_toggle_student_info, bt_toggle_result, bt_toggle_result_summary, bt_toggle_result_keys, bt_toggle_psychomotor;
     private View lyt_expand_student_info, lyt_expand_result_summary, lyt_expand_keys, lyt_expand_psychomotor;
     private TextView school_name, school_address, keys_tv, pschomotor_tv, result_summary_tv, term_result;
     private CircleImageView school_logo;
     private BottomSheetBehavior mBehavior;
     private BottomSheetDialog mBottomSheetDialog;
-    private View bottom_sheet;
 
     private LinearLayout result_layout;
     private String TAG = "mSkola", location = "Mbayande Gboko", email = "cac@gmail.com", website = "www.livingseed.org", full_address = "You know my fulll address dude", calendar = "Resumption = 5th January, 2019; Mid term = NIL; Vacation = We shall post that soon", term, school_id, session, student_reg_no, parent_id;
-    private String class_arm, no_cas, raw_upper_values, raw_lower_values, raw_tags;
     private ArrayList<byte[]> schoolLogo = new ArrayList<>();
     private ArrayList<String> subjects = new ArrayList<>();
     private ArrayList<String> short_codes = new ArrayList<>();
@@ -154,7 +151,7 @@ public class ResultActivity extends AppCompatActivity {
                     //inflate on UI
                     school_name.setText(resultHeader.getSchool_name().toUpperCase());
                     school_address.setText(resultHeader.getLocation());
-                    term_result.setText(term.toUpperCase() + " TERM RESULT");
+                    term_result.setText(String.format("%s TERM RESULT", term.toUpperCase()));
                     location = resultHeader.getLocation();
                     email = resultHeader.getEmail().toLowerCase();
                     website = resultHeader.getWebsite().toLowerCase();
@@ -212,7 +209,7 @@ public class ResultActivity extends AppCompatActivity {
                 String[] data2 = raw_data2.split("<>");
                 String[] data3 = raw_data3.split("<>");
 
-                class_arm = data1[0].toUpperCase();
+                String class_arm = data1[0].toUpperCase();
                 session = data1[1];
                 term = data1[2];
 
@@ -220,15 +217,18 @@ public class ResultActivity extends AppCompatActivity {
                 ParentResultListHeader adapter;
                 ArrayList<NumberResultItem> num = new ArrayList<>();
 
+                NumberResultItem numb = new NumberResultItem();
+                numb.setTitle("REG. NO.");
+                numb.setValue(data3[1]);
+                num.add(numb);
 
-                for (int i = 1; i < data2.length; i++) {
+                for (int i = 2; i < data2.length; i++) {
                     NumberResultItem numbers = new NumberResultItem();
-//                    student_info_titles = student_info_titles + data2[i] + ": \n";
-//                    student_info_values = student_info_values + data3[i] + " \n";
                     numbers.setTitle(data2[i]);
                     numbers.setValue(data3[i]);
                     num.add(numbers);
                 }
+
                 NumberResultItem numbers = new NumberResultItem();
                 numbers.setTitle("CLASS");
                 numbers.setValue(class_arm);
@@ -254,7 +254,7 @@ public class ResultActivity extends AppCompatActivity {
         }
     }
 
-
+    //DONE
     //  3. loads students subject with all its scores
     private class getstdscores extends AsyncTask<String, Integer, String> {
 
@@ -287,11 +287,13 @@ public class ResultActivity extends AppCompatActivity {
                         subjects_with_scores.add(raw_scores.split("<>")[i]);
                         class_average__highest__lowest.add(raw_others.split("<>")[i]);
                     }
-
+                    String psychomotor = "";
                     for (int i = 0; i < raw_psychomotor.split("<>").length; i++) {
                         psychomotor_skills.add(raw_psychomotor.split("<>")[i].split(";")[0]);
                         psychomotor_values.add(raw_psychomotor.split("<>")[i].split(";")[1]);
+                        psychomotor = psychomotor + raw_psychomotor.split("<>")[i].split(";")[0] + " : " + raw_psychomotor.split("<>")[i].split(";")[1];
                     }
+                    pschomotor_tv.setText(psychomotor);
 
                 } catch (Exception ex) {
                     ex.printStackTrace();
@@ -338,15 +340,15 @@ public class ResultActivity extends AppCompatActivity {
                 String data[] = text.split("<>");
 
                 String raw_grades = data[0];
-                raw_upper_values = data[1];
-                raw_lower_values = data[2];
+                String raw_upper_values = data[1];
+                String raw_lower_values = data[2];
 
                 for (int i = 0; i < raw_upper_values.split(";").length; i++) {
                     lower_limit.add(raw_lower_values.split(";")[i]);
                     higher_limit.add(raw_upper_values.split(";")[i]);
                 }
 
-                raw_tags = data[3];
+                String raw_tags = data[3];
                 int no_grades = raw_grades.split(";").length;
                 String grade_text = "";
                 for (int i = 0; i < no_grades; i++) {
@@ -392,7 +394,7 @@ public class ResultActivity extends AppCompatActivity {
                 String data[] = text.split("<>");
 
                 String raw_subjects = data[0], raw_short_codes = data[1];
-                no_cas = data[2];
+                String no_cas = data[2];
                 result.setNoCas(Integer.valueOf(no_cas) + 6); // for exam, total, average....grade
                 int no_subjects = raw_subjects.split(";").length;
                 Log.i(TAG, "no subjects = " + String.valueOf(no_subjects));
@@ -911,6 +913,8 @@ public class ResultActivity extends AppCompatActivity {
         school_logo = findViewById(R.id.logo);
         result_summary_tv = findViewById(R.id.result_summary);
         keys_tv = findViewById(R.id.keys);
+        pschomotor_tv = findViewById(R.id.psychomotor_title);
+
         // nested scrollview
         nested_scroll_view = findViewById(R.id.nested_content);
         loading = findViewById(R.id.loading);
@@ -919,15 +923,12 @@ public class ResultActivity extends AppCompatActivity {
         result_layout = findViewById(R.id.result);
 
         school_name = findViewById(R.id.school_name);
-        school_name.setText("Calvary Arrows College");
         school_address = findViewById(R.id.address);
         school_address.setText(full_address);
         term_result = findViewById(R.id.term_result);
-        //TODO: HINT >>> we display the uncertain text first before the certain text
-
 
         // section description
-        bt_toggle_student_info = findViewById(R.id.bt_toggle_student_info);
+        ImageButton bt_toggle_student_info = findViewById(R.id.bt_toggle_student_info);
         lyt_expand_student_info = findViewById(R.id.lyt_expand_student_info);
         bt_toggle_student_info.setOnClickListener(view -> toggleSection(view, lyt_expand_student_info));
 
@@ -936,25 +937,25 @@ public class ResultActivity extends AppCompatActivity {
         lyt_expand_student_info.setVisibility(View.VISIBLE);
 
         // section result summary
-        bt_toggle_result_summary = findViewById(R.id.bt_toggle_result_summary);
+        ImageButton bt_toggle_result_summary = findViewById(R.id.bt_toggle_result_summary);
         lyt_expand_result_summary = findViewById(R.id.lyt_expand_result_summary);
         bt_toggle_result_summary.setOnClickListener(view -> toggleSection(view, lyt_expand_result_summary));
 
         // section keys
-        bt_toggle_result_keys = findViewById(R.id.bt_toggle_keys);
+        ImageButton bt_toggle_result_keys = findViewById(R.id.bt_toggle_keys);
         lyt_expand_keys = findViewById(R.id.lyt_expand_keys);
         bt_toggle_result_keys.setOnClickListener(view -> toggleSection(view, lyt_expand_keys));
 
         // section psychomotor
-        bt_toggle_psychomotor = findViewById(R.id.bt_toggle_psychomotor);
+        ImageButton bt_toggle_psychomotor = findViewById(R.id.bt_toggle_psychomotor);
         lyt_expand_psychomotor = findViewById(R.id.lyt_expand_psychomotor);
         bt_toggle_psychomotor.setOnClickListener(view -> toggleSection(view, lyt_expand_psychomotor));
 
-        bottom_sheet = findViewById(R.id.bottom_sheet);
+        View bottom_sheet = findViewById(R.id.bottom_sheet);
         mBehavior = BottomSheetBehavior.from(bottom_sheet);
 
         // result section description
-        bt_toggle_result = findViewById(R.id.bt_toggle_result);
+        ImageButton bt_toggle_result = findViewById(R.id.bt_toggle_result);
         bt_toggle_result.setOnClickListener(view -> {
             toggleSection1(view);
             if (result_layout.getVisibility() == View.VISIBLE)
@@ -1223,5 +1224,4 @@ public class ResultActivity extends AppCompatActivity {
             return false;
         }
     }
-
 }
